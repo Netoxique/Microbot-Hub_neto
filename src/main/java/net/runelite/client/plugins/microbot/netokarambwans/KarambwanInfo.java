2a0c0ebbd7d69f753ed3709aee0841b5207fadf1
Netoxic <ernesto.reza.torres@gmail.com>
2025-09-06 17:40:34 -0600

    New plugin.

---
+++ src/main/java/net/runelite/client/plugins/microbot/netokarambwans/KarambwanInfo.java
@@ -0,0 +1,15 @@
+package net.runelite.client.plugins.microbot.netokarambwans;
+
+public class KarambwanInfo {
+    public static states botStatus;
+
+
+    public enum states {
+        FISHING,
+        WALKING_TO_RING_TO_BANK,
+        WALKING_TO_BANK,
+        BANKING,
+        WALKING_TO_FISH,
+        }
+
+}
